fe4ec8a507b625019c18adbf2ec590f292939718
salahmohamed <salahmohamed.sm2016@gmail.com>
2024-10-15 23:26:00 +0300

    integration

--- Task1/PsShape.h
+++ Task1/PsShape.h
@@ -12,122 +12,108 @@ struct Scale
 
 float pi = 3.14159265359;
 
-
 void ph(Scale scale)
 {
-	Scale ps = Scale( scale.shiftX + 5 * scale.scaleFactor, scale.shiftY, scale.scaleFactor * 1.1);
-	glBegin(GL_POLYGON);
-	float thikness = 3;
-	glColor3f(0, 0, 0);
-	glVertex2f(ps.X(0), ps.Y(0));
-	glVertex2f(ps.X(thikness), ps.Y(0));
-	glVertex2f(ps.X(thikness), ps.Y(30));
-	glVertex2f(ps.X(0), ps.Y(30));
-	glEnd();
-	glBegin(GL_POLYGON);
-	glColor3f(0, 0, 0);
-	for (int i = 90; i < 270; i++)
-	{
-		float r = 7;
-		float pointx = -r * cos(i * pi / 180) + thikness -1;
-		float pointy = r * sin(i * pi / 180) + 30 - r;
-		glVertex2f(ps.X(pointx), ps.Y(pointy));
-	}
-	glEnd();
-
-	glBegin(GL_POLYGON);
-
-	glColor3f(1, 1, 1);
-	for (int i = 90; i < 270; i++)
-	{
-		float r = 4;
-		float pointx = -r * cos(i * pi / 180) + thikness;
-		float pointy = r * 1.2* sin(i * pi / 180) + 26 - r;
-		glVertex2f(ps.X(pointx), ps.Y(pointy));
-	}
-	glEnd();
-
-	glBegin(GL_TRIANGLE_FAN);
-
-	float shiftX = 20;
-	float shiftY = 20;
-	glColor3f(0, 0, 0);
-	glVertex2f(scale.X(shiftX), scale.Y(shiftY));
-	for (int i = 0; i <= 360; i++)
-	{
-		float r = 5;
-		float pointx = r * cos(i * pi / 180) + shiftX;
-		float pointy = r * 1.5* sin(i * pi / 180) + shiftY;
-		glVertex2f(scale.X(pointx), scale.Y(pointy));
-	}
-	glEnd();
-	glBegin(GL_TRIANGLE_FAN);
-
-	glColor3f(1, 1, 1);
-	shiftX = 21;
-	shiftY = 20;
-	glVertex2f(scale.X(shiftX), scale.Y(shiftY));
-	for (int i = 0; i <= 360; i++)
-	{
-		float r = 4.4;
-		float pointx = r * cos(i * pi / 180) + shiftX;
-		float pointy = r * 1.5 * sin(i * pi / 180) + shiftY;
-		glVertex2f(scale.X(pointx), scale.Y(pointy));
-	}
-	glEnd();
-
-
-
-	glBegin(GL_TRIANGLE_FAN);
-
-	shiftX = 18;
-	shiftY = 6.5;
-	glColor3f(0, 0, 0);
-	glVertex2f(scale.X(shiftX), scale.Y(shiftY));
-	for (int i = 0; i <= 360; i++)
-	{
-		float r = 5;
-		float pointx = r * cos(i * pi / 180) + shiftX;
-		float pointy = r * 1.5 * sin(i * pi / 180) + shiftY;
-		glVertex2f(scale.X(pointx), scale.Y(pointy));
-	}
-	glEnd();
-	glBegin(GL_TRIANGLE_FAN);
-
-	glColor3f(1, 1, 1);
-	shiftX = 17;
-	shiftY = 6.5;
-	glVertex2f(scale.X(shiftX), scale.Y(shiftY));
-	for (int i = 0; i <= 360; i++)
-	{
-		float r = 4.4;
-		float pointx = r * cos(i * pi / 180) + shiftX;
-		float pointy = r * 1.5 * sin(i * pi / 180) + shiftY;
-		glVertex2f(scale.X(pointx), scale.Y(pointy));
-	}
-	glEnd();
-
-
-	Scale erase = Scale(scale.shiftX +10.5 * scale.scaleFactor, scale.shiftY +10.5* scale.scaleFactor, scale.scaleFactor);
-	glBegin(GL_POLYGON);
-	glColor3f(1, 1, 1);
-	glVertex2f(erase.X(12), erase.Y(0));
-	glVertex2f(erase.X(15), erase.Y(5));
-	glVertex2f(erase.X(8), erase.Y(4));
-	glVertex2f(erase.X(12), erase.Y(8));
-	glEnd();
-
-
-	Scale erase2 = Scale(scale.shiftX + 6 * scale.scaleFactor, scale.shiftY + 9 * scale.scaleFactor, scale.scaleFactor);
-	glBegin(GL_POLYGON);
-	glColor3f(1, 1, 1);
-	glVertex2f(erase2.X(14), erase2.Y(2));
-	glVertex2f(erase2.X(10), erase2.Y(6));
-	glVertex2f(erase2.X(7), erase2.Y(1));
-
-	glEnd();
-
+    Scale ps = Scale(scale.shiftX + 5 * scale.scaleFactor, scale.shiftY, scale.scaleFactor * 1.1);
+    glBegin(GL_POLYGON);
+    float thikness = 2;
+    float blackR = 0;
+    float blackG = 0;
+    float blackB = 0;
+    float whiteR = 0.0;
+    float whiteG = 0.53;
+    float whiteB = 0.83;
+    glColor3f(whiteR, whiteG, whiteB);
+    glVertex2f(ps.X(0), ps.Y(0));
+    glVertex2f(ps.X(thikness), ps.Y(0));
+    glVertex2f(ps.X(thikness), ps.Y(30));
+    glVertex2f(ps.X(0), ps.Y(30));
+    glEnd();
+    glBegin(GL_POLYGON);
+    glColor3f(whiteR, whiteG, whiteB);
+    for (int i = 90; i < 270; i++)
+    {
+        float r = 7;
+        float pointx = -r * cos(i * pi / 180) + thikness - 1;
+        float pointy = r * sin(i * pi / 180) + 30 - r;
+        glVertex2f(ps.X(pointx), ps.Y(pointy));
+    }
+    glEnd();
+    glBegin(GL_POLYGON);
+    glColor3f(blackR, blackG, blackB);
+    for (int i = 90; i < 270; i++)
+    {
+        float r = 4;
+        float pointx = -r * cos(i * pi / 180) + thikness;
+        float pointy = r * 1.2 * sin(i * pi / 180) + 26 - r;
+        glVertex2f(ps.X(pointx), ps.Y(pointy));
+    }
+    glEnd();
+    glBegin(GL_TRIANGLE_FAN);
+    float shiftX = 20;
+    float shiftY = 20;
+    glColor3f(whiteR, whiteG, whiteB);
+    glVertex2f(scale.X(shiftX), scale.Y(shiftY));
+    for (int i = 0; i <= 360; i++)
+    {
+        float r = 5;
+        float pointx = r * cos(i * pi / 180) + shiftX;
+        float pointy = r * 1.5 * sin(i * pi / 180) + shiftY;
+        glVertex2f(scale.X(pointx), scale.Y(pointy));
+    }
+    glEnd();
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3f(blackR, blackG, blackB);
+    shiftX = 21;
+    shiftY = 20;
+    glVertex2f(scale.X(shiftX), scale.Y(shiftY));
+    for (int i = 0; i <= 360; i++)
+    {
+        float r = 4.4;
+        float pointx = r * cos(i * pi / 180) + shiftX;
+        float pointy = r * 1.5 * sin(i * pi / 180) + shiftY;
+        glVertex2f(scale.X(pointx), scale.Y(pointy));
+    }
+    glEnd();
+    glBegin(GL_TRIANGLE_FAN);
+    shiftX = 18;
+    shiftY = 6.5;
+    glColor3f(whiteR, whiteG, whiteB);
+    glVertex2f(scale.X(shiftX), scale.Y(shiftY));
+    for (int i = 0; i <= 360; i++)
+    {
+        float r = 5;
+        float pointx = r * cos(i * pi / 180) + shiftX;
+        float pointy = r * 1.5 * sin(i * pi / 180) + shiftY;
+        glVertex2f(scale.X(pointx), scale.Y(pointy));
+    }
+    glEnd();
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3f(blackR, blackG, blackB);
+    shiftX = 17;
+    shiftY = 6.5;
+    glVertex2f(scale.X(shiftX), scale.Y(shiftY));
+    for (int i = 0; i <= 360; i++)
+    {
+        float r = 4.4;
+        float pointx = r * cos(i * pi / 180) + shiftX;
+        float pointy = r * 1.5 * sin(i * pi / 180) + shiftY;
+        glVertex2f(scale.X(pointx), scale.Y(pointy));
+    }
+    glEnd();
+    Scale erase = Scale(scale.shiftX + 10.5 * scale.scaleFactor, scale.shiftY + 10.5 * scale.scaleFactor, scale.scaleFactor);
+    glBegin(GL_POLYGON);
+    glColor3f(blackR, blackG, blackB);
+    glVertex2f(erase.X(12), erase.Y(0));
+    glVertex2f(erase.X(15), erase.Y(5));
+    glVertex2f(erase.X(8), erase.Y(4));
+    glVertex2f(erase.X(12), erase.Y(8));
+    glEnd();
+    Scale erase2 = Scale(scale.shiftX + 6 * scale.scaleFactor, scale.shiftY + 9 * scale.scaleFactor, scale.scaleFactor);
+    glBegin(GL_POLYGON);
+    glColor3f(blackR, blackG, blackB);
+    glVertex2f(erase2.X(14), erase2.Y(2));
+    glVertex2f(erase2.X(10), erase2.Y(6));
+    glVertex2f(erase2.X(7), erase2.Y(1));
+    glEnd();
 }
-
-
-
--- Task1/main.cpp
+++ Task1/main.cpp
@@ -6,35 +6,166 @@
 #include <freeglut.h>
 #include <math.h>
 
+#include <iostream>
+
 struct Scale
 {
-	Scale(float shiftX, float shiftY, float scaleFactor) : shiftX(shiftX), shiftY(shiftY), scaleFactor(scaleFactor) {}
-	float shiftX = 0;
-	float shiftY = 0;
-	float scaleFactor = 1;
-	float X(float x) { return x * scaleFactor + shiftX; }
-	float Y(float y) { return y * scaleFactor + shiftY; }
+    Scale(float shiftX, float shiftY, float scaleFactor) : shiftX(shiftX), shiftY(shiftY), scaleFactor(scaleFactor) {}
+    float shiftX = 0;
+    float shiftY = 0;
+    float scaleFactor = 1;
+    float X(float x) { return x * scaleFactor + shiftX; }
+    float Y(float y) { return y * scaleFactor + shiftY; }
 };
 
-void Square(Scale scale)
+using namespace std;
+float pi = 3.14159265359;
+void ph(Scale scale)
 {
-	glBegin(GL_POLYGON);
-	glColor3b(0, 0, 0);
-	glVertex2f(scale.X(20), scale.Y(20));
-	glVertex2f(scale.X(80), scale.Y(20));
-	glVertex2f(scale.X(80), scale.Y(80));
-	glVertex2f(scale.X(20), scale.Y(80));
+    Scale ps = Scale(scale.shiftX + 5 * scale.scaleFactor, scale.shiftY, scale.scaleFactor * 1.1);
+    glBegin(GL_POLYGON);
+    float thikness = 2;
+    float blackR = 0;
+    float blackG = 0;
+    float blackB = 0;
+    float whiteR = 0.0;
+    float whiteG = 0.53;
+    float whiteB = 0.83;
+    glColor3f(whiteR, whiteG, whiteB);
+    glVertex2f(ps.X(0), ps.Y(0));
+    glVertex2f(ps.X(thikness), ps.Y(0));
+    glVertex2f(ps.X(thikness), ps.Y(30));
+    glVertex2f(ps.X(0), ps.Y(30));
+    glEnd();
+    glBegin(GL_POLYGON);
+    glColor3f(whiteR, whiteG, whiteB);
+    for (int i = 90; i < 270; i++)
+    {
+        float r = 7;
+        float pointx = -r * cos(i * pi / 180) + thikness - 1;
+        float pointy = r * sin(i * pi / 180) + 30 - r;
+        glVertex2f(ps.X(pointx), ps.Y(pointy));
+    }
+    glEnd();
+    glBegin(GL_POLYGON);
+    glColor3f(blackR, blackG, blackB);
+    for (int i = 90; i < 270; i++)
+    {
+        float r = 4;
+        float pointx = -r * cos(i * pi / 180) + thikness;
+        float pointy = r * 1.2 * sin(i * pi / 180) + 26 - r;
+        glVertex2f(ps.X(pointx), ps.Y(pointy));
+    }
+    glEnd();
+    glBegin(GL_TRIANGLE_FAN);
+    float shiftX = 20;
+    float shiftY = 20;
+    glColor3f(whiteR, whiteG, whiteB);
+    glVertex2f(scale.X(shiftX), scale.Y(shiftY));
+    for (int i = 0; i <= 360; i++)
+    {
+        float r = 5;
+        float pointx = r * cos(i * pi / 180) + shiftX;
+        float pointy = r * 1.5 * sin(i * pi / 180) + shiftY;
+        glVertex2f(scale.X(pointx), scale.Y(pointy));
+    }
+    glEnd();
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3f(blackR, blackG, blackB);
+    shiftX = 21;
+    shiftY = 20;
+    glVertex2f(scale.X(shiftX), scale.Y(shiftY));
+    for (int i = 0; i <= 360; i++)
+    {
+        float r = 4.4;
+        float pointx = r * cos(i * pi / 180) + shiftX;
+        float pointy = r * 1.5 * sin(i * pi / 180) + shiftY;
+        glVertex2f(scale.X(pointx), scale.Y(pointy));
+    }
+    glEnd();
+    glBegin(GL_TRIANGLE_FAN);
+    shiftX = 18;
+    shiftY = 6.5;
+    glColor3f(whiteR, whiteG, whiteB);
+    glVertex2f(scale.X(shiftX), scale.Y(shiftY));
+    for (int i = 0; i <= 360; i++)
+    {
+        float r = 5;
+        float pointx = r * cos(i * pi / 180) + shiftX;
+        float pointy = r * 1.5 * sin(i * pi / 180) + shiftY;
+        glVertex2f(scale.X(pointx), scale.Y(pointy));
+    }
+    glEnd();
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3f(blackR, blackG, blackB);
+    shiftX = 17;
+    shiftY = 6.5;
+    glVertex2f(scale.X(shiftX), scale.Y(shiftY));
+    for (int i = 0; i <= 360; i++)
+    {
+        float r = 4.4;
+        float pointx = r * cos(i * pi / 180) + shiftX;
+        float pointy = r * 1.5 * sin(i * pi / 180) + shiftY;
+        glVertex2f(scale.X(pointx), scale.Y(pointy));
+    }
+    glEnd();
+    Scale erase = Scale(scale.shiftX + 10.5 * scale.scaleFactor, scale.shiftY + 10.5 * scale.scaleFactor, scale.scaleFactor);
+    glBegin(GL_POLYGON);
+    glColor3f(blackR, blackG, blackB);
+    glVertex2f(erase.X(12), erase.Y(0));
+    glVertex2f(erase.X(15), erase.Y(5));
+    glVertex2f(erase.X(8), erase.Y(4));
+    glVertex2f(erase.X(12), erase.Y(8));
+    glEnd();
+    Scale erase2 = Scale(scale.shiftX + 6 * scale.scaleFactor, scale.shiftY + 9 * scale.scaleFactor, scale.scaleFactor);
+    glBegin(GL_POLYGON);
+    glColor3f(blackR, blackG, blackB);
+    glVertex2f(erase2.X(14), erase2.Y(2));
+    glVertex2f(erase2.X(10), erase2.Y(6));
+    glVertex2f(erase2.X(7), erase2.Y(1));
+    glEnd();
+}
+void drawEquilateralTriangle(Scale scale, int r, int g, int b) {
+	glBegin(GL_TRIANGLES);
+	glColor3ub(r, g, b);
+
+	// We pass the x and the y and it will scale and shift according to
+	// the parameters we gave the Scale when creating the triangle
+	//Use sqrt( (x2 - x1)^2 + (y2 - y1)^2) ) to check equivalence of sides
+	glVertex2f(scale.X(0.0f), scale.Y(scale.scaleFactor)); // Top (0, scaleFactor)
+	glVertex2f(scale.X(-scale.scaleFactor * 0.5f), scale.Y(-scale.scaleFactor * 0.5f)); // Bottom left (-scaleFactor*0.5, -scaleFactor*0.5)
+	glVertex2f(scale.X(scale.scaleFactor * 0.5f), scale.Y(-scale.scaleFactor * 0.5f)); // Bottom right (scaleFactor*0.5, -scaleFactor*0.5)
 	glEnd();
 }
+void drawCircle(Scale scale, int r, int g, int b) {
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3ub(r, g, b);
+    float radius = scale.scaleFactor;
+    int numSegments = 150;
+    float aspectRatio = 700.0f / 500.0f; // Calculate the aspect ratio of the screen
+    glVertex2f(scale.X(0), scale.Y(0)); // Scale the y-coordinate by the aspect ratio
+    for (int i = 0; i <= numSegments; i++) {
+        float theta = 2.0f * 3.1415926f * float(i) / float(numSegments);
+        float x = radius * cosf(theta);
+        float y = radius * sinf(theta) * aspectRatio; // Scale the y-coordinate by the aspect ratio
+        glVertex2f(scale.X(x), scale.Y(y));
+    }
+    glEnd();
+}
+
 // Drawing routine.
 void drawScene()
 {
-	float shiftX = 10;
-	float shiftY = 10;
-	float scaleFactor = 0.5;
-
     glClear(GL_COLOR_BUFFER_BIT);
-	Square(Scale(shiftX,shiftY,scaleFactor));
+	float scaleps = 0.8;
+	ph(Scale(40,60,scaleps));
+	float scaleCircle = 3.1;
+	drawCircle(Scale(35, 32.5, scaleCircle), 255, 69, 0); // Outer Circle (Orange)
+	drawCircle(Scale(35, 32.5, scaleCircle * 0.85), 0, 0, 0); // Inner Circle (Black)
+
+	float scaleTriangle = 4;
+	drawEquilateralTriangle(Scale(15, 30, scaleTriangle), 41, 174, 147);
+	drawEquilateralTriangle(Scale(15, 30, scaleTriangle * 0.8), 0, 0, 0); // Color of background
     glFlush();
 }
 
@@ -42,7 +173,7 @@ void drawScene()
 void setup(void)
 {
 	//the clearing color of the opengl window (background)
-	glClearColor(1, 1, 1, 1);
+	glClearColor(0, 0, 0, 1);
 }
 
 // OpenGL window reshape routine.
@@ -92,7 +223,7 @@ int main(int argc, char** argv)
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
 
 	// set the initial size of the OpenGL window and the location of its top left corner on the computer screen
-	glutInitWindowSize(1000, 700);
+	glutInitWindowSize(800, 500);
 	glutInitWindowPosition(0, 0);
 
 	// creates the OpenGL context and its associated window with the specified string parameter as title.
